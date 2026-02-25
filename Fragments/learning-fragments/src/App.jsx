d7195d07065c1bdfc832cf4c74c5ad7cd8259757
rjvrr <rajbirsingh1429@gmail.com>
2026-02-25 21:16:54 +0530

    Added conatiner

--- Fragments/learning-fragments/src/App.jsx
+++ Fragments/learning-fragments/src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
 import React from "react";
 import './App.css';
 import FoodItems from './components/foodItems';
+import Container from "./components/container";
 
 function App() {
   let foods = ["Roti" , "Dal" , "Green vegetable" , "Fruits" , "Salad"];
@@ -18,12 +18,42 @@ function App() {
         </ul>
     </>*/
     
-    <React.Fragment> 
+    /*<React.Fragment> 
       <h1> Healthy Food </h1>
 
       <FoodItems food={foods}> </FoodItems>
         
-    </React.Fragment>
+    </React.Fragment>*/
+
+    
+      /*<>
+        <h1> Healthy Food </h1>
+          <ul class="list-group">
+            <li class="list-group-item">Roti</li>
+            <li class="list-group-item">Dal</li>
+            <li class="list-group-item">Green vegetable</li>
+            <li class="list-group-item">Fruits</li>
+            <li class="list-group-item">Salad</li>
+          </ul>
+      </>*/
+      /*<React.Fragment>
+        <h1> Healthy Food </h1>
+      
+        <FoodItems food={foods}> </FoodItems>
+          
+      </React.Fragment>*/
+      <>
+        <Container>
+          <h1> Healthy Food </h1>
+
+          <FoodItems food={foods}> </FoodItems>
+
+        </Container>
+        
+        <Container> 
+          Above is the list of healthy foods that one should eat daily.
+        </Container>
+      </>
   )
 }
 
